dfa2b68aad999fbf9a87b36573fd1df73b034d11
2wenty1ne <sirdarkvic@gmail.com>
2024-12-02 01:50:26 +0100

    Solved in zig too

--- Day1/day1.zig
+++ Day1/day1.zig
@@ -1,20 +1,25 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const sort = std.mem.sort;
+const expect = std.testing.expect;
+const ArrayList = std.ArrayList;
 
-const LType: type = u8; 
+const sort = std.mem.sort;
+const dbg = std.debug;
+
+const LType: type = i64;
 
 pub fn main() !void {
-    //const Self = @This();
     const writer = std.io.getStdOut().writer();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // &[_]LType{ 3, 4, 2, 1, 3, 3}
     //? Get file
-    const file = try std.fs.cwd().openFile("testData.txt", .{.mode=.read_only},);
+    const file = try std.fs.cwd().openFile(
+        "Data.txt",
+        .{ .mode = .read_only },
+    );
     defer file.close();
 
     //? Get file size
@@ -22,28 +27,95 @@ pub fn main() !void {
     const fileSize = fileStats.size;
     try writer.print("File Size: {d} \n", .{fileSize});
 
+    //? Read Data into buffer
+    var reader = file.reader();
 
-    //? Read data into buffer
-    const buffer = try allocator.alloc(u8, fileSize);
-    defer allocator.free(buffer);
-    try file.seekTo(0);
-    _ = try file.readAll(buffer);
+    const data: []u8 = try reader.readAllAlloc(allocator, fileSize * 2);
+    defer allocator.free(data);
 
-    try writer.print("File Content: \n{s}\n", .{buffer});
-
-    
-
-    // sort(u8, list1, {}, comptime.sort.desc(u8));
+    //? Use data
+    var lines = std.mem.split(u8, data, "\n");
 
 
-    try writer.print("Finished!", .{});
+    var firstList = ArrayList(LType).init(allocator);
+    defer firstList.deinit();
+
+    var secondList = ArrayList(LType).init(allocator);
+    defer secondList.deinit();
+
+
+    while (lines.next()) |uncleanLine| {
+        const line = std.mem.trim(u8, uncleanLine, "\r");
+
+        var nums = std.mem.split(u8, line, "   ");
+
+        const firstValue = nums.next() orelse unreachable;
+        const secondValue = nums.next() orelse unreachable;
+
+        const num1 = try std.fmt.parseInt(LType, firstValue, 10);
+        const num2 = try std.fmt.parseInt(LType, secondValue, 10);
+
+
+        try firstList.append(num1);
+        try secondList.append(num2);
+    }
+
+
+    const firstArray = try firstList.toOwnedSlice();
+    defer allocator.free(firstArray);
+    const secondArray = try secondList.toOwnedSlice();
+    defer allocator.free(secondArray);
+
+    std.mem.sort(LType, firstArray, {}, comptime std.sort.asc(LType));
+    std.mem.sort(LType, secondArray, {}, comptime std.sort.asc(LType));
+
+    var sum: LType = 0;
+
+    for (firstArray, 0..) |firstElement, index| {
+        sum = sum + abs(firstElement - secondArray[index]);
+    }
+
+    var simSum: LType = 0;
+
+    for (firstArray) |firstElement| {
+        var counter: LType = 0;
+        for (secondArray) |secondElement| {
+            if (firstElement == secondElement) {
+                counter = counter + 1;
+            }
+        }
+        simSum = simSum + (counter * firstElement);
+    }
+
+
+    try writer.print("Result 1: {d} \n", .{sum});
+    try writer.print("Result 2: {d} \n", .{simSum});
+
+    try writer.print("\nFinished!", .{});
 }
 
 
-// pub fn printArrL(list: ArrayList(LType)) !void {
-//     const writer = std.io.getStdOut().writer();
+pub fn abs(num: LType) LType {
+    if (num < 0) {
+        return num * -1;
+    }
+    else return num;
+}
 
-//     for (list.items) |x| {
-//         try writer.print("{d} \n",.{x});
-//     }
-// }
+
+pub fn printU64(arr: []LType) !void {
+    const writer = std.io.getStdOut().writer();
+
+    for (arr) |x| {
+        try writer.print("{d} \n", .{x});
+    }
+}
+
+
+pub fn printArrL(list: ArrayList(LType)) !void {
+    const writer = std.io.getStdOut().writer();
+
+    for (list.items) |x| {
+        try writer.print("{d} \n", .{x});
+    }
+}
