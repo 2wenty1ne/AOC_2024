eb91506eae0dc3b014652a91a8d971c3f4dc01e9
2wenty1ne <sirdarkvic@gmail.com>
2024-12-03 02:52:37 +0100

    ohje

--- Day2/day2.go
+++ Day2/day2.go
@@ -24,62 +24,86 @@ func main() {
 
 	scanner := bufio.NewScanner(file);
 
+	//? Go through file line by line
 	for scanner.Scan() {
 		report := scanner.Text();
 		levels := strings.Split(report, " ");
 
-		var ascending bool
-		var unsafe bool
-		
-		//? Check the first two to determine ascending / descending
-		first, err := strconv.Atoi(levels[0]);
-			if err != nil {
-				panic(err);
-			}
-		second, err := strconv.Atoi(levels[1]);
-			if err != nil {
-				panic(err);
-			}
-		
-		//? Determine ascending / descending
-		if (first < second) {
-			ascending = true;
+		var safe bool
+
+		for i := 0; i < len(levels); i++ {
+			copyLevels := append([]string{}, levels...)
+			copyLevels = append(copyLevels[:i], copyLevels[i+1:]...)
+			unsafeCounter := checkReport(copyLevels)
+
+			if unsafeCounter <= 1 {safe = true}
 		}
 
-		//? Check if first two are not meeting difference requirement
-		firstDiff := abs(first - second);
-		if !(firstDiff >= 1 && firstDiff <= 3) {
-			continue;
-		}
+		if safe {amountSafeLevels++}
 
-		//? Check remaining levels in reports
-		for i := 1; i < (len(levels) - 1); i++ {
-			element, err := strconv.Atoi(levels[i]);
-			if err != nil {
-				panic(err);
-			}
-
-			compare, err := strconv.Atoi(levels[i+1]);
-			if err != nil {
-				panic(err);
-			}
-
-			if ascending {
-				if !(element < compare) {unsafe = true}
-			} else {
-				if !(element > compare) {unsafe = true}
-			}
-
-			diff := abs(element - compare);
-			if !(diff >= 1 && diff <= 3) {unsafe = true}
-		}
-		if !unsafe {
-			amountSafeLevels = amountSafeLevels + 1;
-		}
+		// if !unsafe {
+		// 	amountSafeLevels = amountSafeLevels + 1;
+		// }
 	}
 
 	fmt.Printf("Result: %d \n", amountSafeLevels);
 
+}
+
+
+func removeByIndex(arr []string, index int) []string {
+	return append(arr[:index], arr[index+1:]...)
+}
+
+
+func checkReport(levels []string) int {
+	var ascending bool
+	counter := 0 
+
+	//? Check the first two to determine ascending / descending
+	first, err := strconv.Atoi(levels[0]);
+		if err != nil {
+			panic(err);
+		}
+	second, err := strconv.Atoi(levels[1]);
+		if err != nil {
+			panic(err);
+		}
+	
+	//? Determine ascending / descending
+	if (first < second) {
+		ascending = true;
+	}
+
+	//? Check if first two are not meeting difference requirement
+	firstDiff := abs(first - second);
+	if !(firstDiff >= 1 && firstDiff <= 3) {
+		counter ++
+	}
+
+	//? Check remaining levels in reports
+	for i := 1; i < (len(levels) - 1); i++ {
+		element, err := strconv.Atoi(levels[i]);
+		if err != nil {
+			panic(err);
+		}
+
+		compare, err := strconv.Atoi(levels[i+1]);
+		if err != nil {
+			panic(err);
+		}
+
+		if ascending {
+			if !(element < compare) {counter ++}
+		} else {
+			if !(element > compare) {counter ++}
+		}
+
+		diff := abs(element - compare);
+		if !(diff >= 1 && diff <= 3) {counter ++}
+	}
+
+	return counter
 }
 
 
